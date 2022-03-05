a64994db1c47671b84982035eeec129d659e64d7
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-05 19:47:21 +0800

    fixed response

--- vulnerable-sample/server.js
+++ vulnerable-sample/server.js
@@ -3,5 +3,10 @@ const unnamed = require("unnamed-js");
 const server = unnamed({ port: 3000 });
 
 server.GET("/", (req, res) => {
-  res.send(`<html><body>Hello</body><input value="${req.query.name}"/></html>`);
+  const response =
+    req.query && req.query.hasOwnProperty("name")
+      ? `<html><body>Hello</body><input value="${req.query.name}"/></html>`
+      : `<html><body>Hello</body><input /></html>`;
+
+  res.send(response);
 });
