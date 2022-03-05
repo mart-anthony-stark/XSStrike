a10f34c62a65820955ff6f8185ddd850331d06ec
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-05 20:59:09 +0800

    fixed response

--- vulnerable-sample/express/app.js
+++ vulnerable-sample/express/app.js
@@ -3,7 +3,12 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(`<html><body>Hello</body><input value="${req.query.name}"/></html>`);
+  const response =
+    req.query && req.query.hasOwnProperty("name")
+      ? `<html><body>Hello</body><input value="${req.query.name}"/></html>`
+      : `<html><body>Hello</body><input /></html>`;
+
+  res.send(response);
 });
 
 app.listen(3000, () => {
