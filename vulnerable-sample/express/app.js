70a965cd0e90985c17c0e34267bba70b0d73bee0
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-05 21:41:20 +0800

    add post route

--- vulnerable-sample/express/app.js
+++ vulnerable-sample/express/app.js
@@ -3,16 +3,26 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  const response =
-    req.query && req.query.hasOwnProperty("name")
-      ? `<html><body>Hello</body><input value="${req.query.name}"/></html>`
-      : `<html><body>Hello</body><input /></html>`;
+// app.get("/", (req, res) => {
+//   const response =
+//     req.query && req.query.hasOwnProperty("name")
+//       ? `<html><body>Hello</body><input value="${req.query.name}"/></html>`
+//       : `<html><body>Hello</body><input /></html>`;
 
-  res.send(response);
+//   res.send(response);
+// });
+
+app.get("/", (req, res) => {
+  res.render("register");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.render("register");
 });
 
 app.listen(3000, () => {
