4e7f3014879800c89b18aa13c7ffa6c67c3b50aa
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-05 21:29:38 +0800

    addview engine

--- vulnerable-sample/express/app.js
+++ vulnerable-sample/express/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.set("view-engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get("/", (req, res) => {
   const response =
     req.query && req.query.hasOwnProperty("name")
